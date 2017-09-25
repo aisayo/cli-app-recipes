abfb2993398334932cdf4cc3b2bb3f2fe49dfa06
aisayo <aisayo@icloud.com>
2017-09-25 23:40:17 +0000

    added menu method

--- lib/keto_recipes/recipecontroller.rb
+++ lib/keto_recipes/recipecontroller.rb
@@ -2,6 +2,7 @@ class RecipeController
 
   def call
     options_list
+    menu
   end
 
   def options_list
@@ -9,6 +10,21 @@ class RecipeController
     puts "1. Breakfast"
     puts "2. Lunch"
     puts "3. Dinner"
+  end
+
+  def menu
+    puts "Enter the number of your selection or type 'exit' to quit:"
+
+    input = gets.strip
+
+    case input
+    when "1"
+      puts "recipe 1"
+    when "2"
+      puts "recipe 2"
+    when "3"
+      puts "recipe 3"
+    end
 
   end
 
