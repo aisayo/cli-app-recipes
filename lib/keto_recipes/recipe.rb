ec0e831699a94591a1ee465e25c69af4bb20e980
aisayo <aisayo@icloud.com>
2017-09-29 18:18:28 +0000

    instantiated and gave params

--- lib/keto_recipes/recipe.rb
+++ lib/keto_recipes/recipe.rb
@@ -1,42 +1,40 @@
 require 'pry'
 
 class KetoRecipes::Recipe
-  attr_accessor :title, :post_date, :category
-
+  attr_accessor :title, :post_date, :category, :url
   @@all = []
+
+  def initialize(title, post_date, category, url)
+    @title = title
+    @post_date = post_date
+    @category = category
+    @url = url
+    @@all << self
+  end
 
   def self.all
     @@all
   end
 
   def save
-    @@all << self
+    @@all << self.scrape_info
   end
 
   def self.scrape_info
 
-    breakfast = self.new
-    breakfast.title = "title"
-    breakfast.post_date = "date 1"
-    breakfast.category = "breakfast"
+    breakfast = self.new("title", "date 1", "breakfast", "url")
 
-    lunch = self.new
-    lunch.title = "title"
-    lunch.post_date = "date 2"
-    lunch.category = "lunch"
+    lunch = self.new("title", "date 1", "lunch", "url")
 
-    dinner = self.new
-    dinner.title = "title"
-    dinner.post_date = "date 3"
-    dinner.category = "dinner"
+    dinner = self.new("title", "date 1", "dinner", "url")
 
     [breakfast, lunch, dinner]
 
   end
 
+
 #category selector div.r-swiper-data
 #title : div.articleTitle
 #link : https://www.ruled.me/keto-recipes/
-
 
 end
--- lib/keto_recipes/recipecontroller.rb
+++ lib/keto_recipes/recipecontroller.rb
@@ -11,36 +11,26 @@ class KetoRecipes::RecipeController
 
   def list_categories
     puts "What type of meal are you looking for?"
-    KetoRecipes::Recipe.all.each.with_index(1) do |category, i|
+    KetoRecipes::Recipe.all.each.with_index(1) do |recipe, i|
       puts "#{i}. #{recipe.category}"
     end
-    puts ""
   end
-    # puts "What are you looking for?"
-    # puts "1. Breakfast"
-    # puts "2. Lunch"
-    # puts "3. Dinner"
-    # puts "Enter the number of your selection or type 'exit' to quit."
 
 
   def list_recipes
 
     input = nil
 
-    # recipes = Recipe
-    # recipes.each.with_index(1) do |recipe, i|
-    #   recipe = "#{i}. #{recipe.name} - #{recipe.url}"
-    #   recipe
     while input != "exit"
     input = gets.strip
     if input.to_i == 1
-      puts recipe
+      puts "1"
     elsif input.to_i == 2
-      puts recipe
+      puts "2"
     elsif input.to_i == 3
-      puts recipe
+      puts "3"
     elsif input == "list"
-      options_list
+      list_categories
     else
       puts "Please type a valid entry. To access the list again, type list or leave the program type 'exit':"
       end
